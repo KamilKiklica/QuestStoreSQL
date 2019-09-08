e49cbe3f871afa62182d263a4ab20cda35f6a8a2
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2019-09-09 00:54:37 +0200

    Clean Spring

---
+++ src/main/java/com/kam/prze/woj/queststore/controller/Hello.java
@@ -0,0 +1,15 @@
+package com.kam.prze.woj.queststore.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class Hello {
+	@RequestMapping("/")
+	@ResponseBody
+	public String hello() {
+		return "Hello Spring Boot";
+	}
+}
+
---
+++ src/test/java/com/kam/prze/woj/queststore/QueststoreApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kam.prze.woj.queststore;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QueststoreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
